fe96de0b852b87041f0d2736375a579edb045a25
David Grammatico <david.lugdunum@gmail.com>
2022-09-30 22:24:16 +0200

    add profil class

---
+++ lib/model/profil.dart
@@ -0,0 +1,44 @@
+class Profil {
+  String firstName;
+  String lastName;
+  int age;
+  double taille;
+  bool genre;
+  List<String> hobbies;
+  String languageFavorite;
+  String secret;
+
+  Profil(
+      {required this.firstName,
+      required this.lastName,
+      required this.age,
+      required this.taille,
+      required this.genre,
+      required this.hobbies,
+      required this.languageFavorite,
+      required this.secret});
+
+  String setFirstName() {
+    return "$firstName $lastName";
+  }
+
+  String setAge() {
+    if (age > 1) {
+      return "$age ans";
+    } else {
+      return "$age an";
+    }
+  }
+
+  String setTaille() {
+    if (taille >= 2) {
+      return "${taille.toInt()} cms";
+    } else {
+      return "${taille.toInt()} cm";
+    }
+  }
+
+  String setSexe() {
+    return genre ? "Masculin" : "Féminin";
+  }
+}
