1cf1cc06f89a4f12f2a6891b56e5940241503a2b
David Grammatico <david.lugdunum@gmail.com>
2022-09-30 22:09:55 +0200

    add home_view

---
+++ lib/view/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
